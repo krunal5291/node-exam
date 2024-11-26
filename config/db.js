f02ab78911e1db9dc4fddb69341139518f1f298b
krunal5291 <krunalkalathiya11@gmail.com>
2024-11-26 10:22:27 +0530

    ok

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose  = require("mongoose")
 
 const connect = async() =>{
-    await mongoose.connect("mongodb://127.0.0.1:27017/task")
+    await mongoose.connect("mongodb://localhost:27017")
     console.log("mongodb is connect")
 }
 
